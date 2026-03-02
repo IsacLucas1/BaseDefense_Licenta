0f05b3ceb87516f42c06a6396094b32d4b6f917e
IsacLucas1 <lucas.isac@student.upt.ro>
2026-03-02 17:13:19 +0200

    Mecanica de construire a zidurilor + posibilitate de distrugere zid de catre constructor (de test) + reparare zid

--- Assets/Scripts/ConstructorPlayer.cs
+++ Assets/Scripts/ConstructorPlayer.cs
@@ -31,7 +31,7 @@ public class ConstructorPlayer : BasePlayer
             }
         }
     }
-
+    
     public override void AdaugaLemn(int cantitate)
     {
         if (IsServer)
@@ -40,5 +40,119 @@ public class ConstructorPlayer : BasePlayer
              base.AdaugaLemn(lemnAdaugat);
         }
        
+    }
+    
+    protected override void Update()
+    {
+        base.Update();
+        
+        if (!IsOwner || isDead || isRecalling)
+        {
+            return;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            IncearcaSaConstruiascaSauRepare();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            IncearcaSaDistrugaZid();
+        }
+    }
+    
+    private void IncearcaSaConstruiascaSauRepare()
+    {
+        Ray ray = new Ray(cameraCap.transform.position, cameraCap.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, distantaAdunare, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Collide))
+        {
+            Zid zid = hit.collider.GetComponent<Zid>();
+
+            if (zid != null)
+            {
+                if (zid.viata.Value < zid.viataMax)
+                {
+                    int costConstructie = 10;
+                                    
+                    if(lemn.Value >= costConstructie)
+                    {
+                        Debug.Log("Am trimis comanda la server sa construiasca zidul!");
+                        ConstruiesteZidServerRpc(zid.NetworkObjectId, costConstructie);
+                    }
+                    else
+                    {
+                        Debug.Log("Nu ai destul lemn! Ai " + lemn.Value + " dar iti trebuie " + costConstructie);
+                    }
+                }
+                else
+                {
+                    Debug.Log("Acest zid este deja la 100% viață!");
+                }
+            }
+            else
+            {
+                Debug.Log("Mă uit la " + hit.collider.name + " dar nu este un Zid.");
+            }
+        }
+        else
+        {
+            Debug.Log("Nu mă uit la niciun obiect sau sunt prea departe.");
+        }
+    }
+
+    [ServerRpc]
+    private void ConstruiesteZidServerRpc(ulong zidId, int cost)
+    {
+        if (lemn.Value >= cost)
+        {
+            if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(zidId, out var obj))
+            {
+                Zid zid = obj.GetComponent<Zid>();
+                if (zid != null && zid.viata.Value < zid.viataMax)
+                {
+                    lemn.Value -= cost;
+                    zid.ConstruiesteSauReparaServerRpc(20);
+                    Debug.Log("Zidul are: " + zid.viata.Value + " / " + zid.viataMax);
+                }
+            }
+        }
+    }
+    
+    private void IncearcaSaDistrugaZid()
+    {
+        Ray ray = new Ray(cameraCap.transform.position, cameraCap.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, distantaAdunare))
+        {
+            Zid zid = hit.collider.GetComponent<Zid>();
+
+            if (zid != null && zid.viata.Value > 0)
+            {
+                int damageDistrugere = 12;
+                Debug.Log($"Am lovit zidul! I-am dat {damageDistrugere} damage.");
+                DamageZidServerRpc(zid.NetworkObjectId, damageDistrugere);
+            }
+            else if(zid != null && zid.viata.Value <= 0)
+            {
+                Debug.Log("Acest zid este deja distrus!");
+            }
+            else
+            {
+                Debug.Log("Mă uit la " + hit.collider.name + " dar nu este un Zid.");
+            }
+        }
+    }
+
+    [ServerRpc]
+    private void DamageZidServerRpc(ulong zidId, int damage)
+    {
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(zidId, out var obj))
+        {
+            Zid zid = obj.GetComponent<Zid>();
+            if (zid != null)
+            {
+                zid.PrimesteDamage(damage);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Zid.cs
@@ -0,0 +1,125 @@
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.Analytics;
+using System.Collections;
+
+public class Zid : NetworkBehaviour
+{
+    [Header("Setari Zid")]
+    public int viataMax = 100;
+
+    public NetworkVariable<int> viata = new NetworkVariable<int>(0);
+    
+    private Collider zidCollider;
+    private Renderer wallRenderer;
+    private Color culoareOriginala;
+    
+    private void Awake()
+    {
+        zidCollider = GetComponent<Collider>();
+        wallRenderer = GetComponentInChildren<Renderer>();
+        if (wallRenderer != null)
+        {
+            if (wallRenderer.materials[0].HasProperty("_BaseColor"))
+            {
+                culoareOriginala = wallRenderer.materials[0].GetColor("_BaseColor");
+            }
+            else
+            {
+                culoareOriginala = wallRenderer.materials[0].color;
+            }
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        viata.OnValueChanged += LaSchimbareViata;
+        ActualizeazaZid(viata.Value);
+    }
+    
+    public override void OnNetworkDespawn()
+    {
+        viata.OnValueChanged -= LaSchimbareViata;
+    }
+    
+    private void LaSchimbareViata(int oldValue, int newValue)
+    {
+        ActualizeazaZid(newValue);
+    }
+    
+    private void ActualizeazaZid(int viataCurenta)
+    {
+        if (viataCurenta <= 0)
+        {
+            if (zidCollider != null)
+            {
+                zidCollider.isTrigger = true;
+            }
+            SeteazaOpacitate(0.3f, false);
+        }
+        else if (viataCurenta >= viataMax)
+        {
+            if (zidCollider != null)
+            {
+                zidCollider.isTrigger = false;
+            }
+            SeteazaOpacitate(1f, true);
+        }
+        else
+        {
+            if (zidCollider != null)
+            {
+                zidCollider.isTrigger = false;
+            }
+            float procent = (float)viataCurenta / viataMax;
+            float alpha = Mathf.Lerp(0.3f, 1f, procent);
+            SeteazaOpacitate(alpha, false);
+        }
+    }
+
+    private void SeteazaOpacitate(float alpha, bool devineOpac)
+    {
+        if (wallRenderer != null)
+        {
+            foreach (Material mat in wallRenderer.materials)
+            {
+                Color c = culoareOriginala;
+                c.a = alpha;
+
+                if (mat.HasProperty("_BaseColor"))
+                {
+                    mat.SetColor("_BaseColor", c);
+                }
+
+                if (devineOpac)
+                {
+                    mat.SetFloat("_Surface", 0f);
+                    mat.SetFloat("_ZWrite", 1f);
+                    mat.renderQueue = 2000;
+                }
+                else
+                {
+                    mat.SetFloat("_Surface", 1f);
+                    mat.SetFloat("_ZWrite", 0f);
+                    mat.renderQueue = 3000;
+                }
+            }
+            
+        }
+    }
+    
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    public void ConstruiesteSauReparaServerRpc(int viataAdaugata)
+    {
+        viata.Value = Mathf.Min(viata.Value + viataAdaugata, viataMax);
+    }
+    
+    public void PrimesteDamage(int damage)
+    {
+        if (!IsServer)
+        {
+            return;
+        }
+        viata.Value = Mathf.Max(viata.Value - damage, 0);
+    }
+}
